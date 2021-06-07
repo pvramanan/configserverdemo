e818d264aa73390c0940fc80b001c3fcf4d0ab3a
Prasanna Vengataramana Gunasekaran <35519986+pvramanan@users.noreply.github.com>
2021-06-07 23:06:11 +0530

    with security changes

---
+++ src/main/java/com/pvr/configserverdemo/config/SecurityConfiguration.java
@@ -0,0 +1,14 @@
+package com.pvr.configserverdemo.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+//@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity httpSecurity) throws Exception {
+    httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+  }
+
+}
